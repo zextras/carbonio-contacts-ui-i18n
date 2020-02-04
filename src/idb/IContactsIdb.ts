aecb31827d2af9afbf24523e7ecca2dc67dd4371
Michele Olivo <michele.olivo@zextras.com>
2020-02-04 17:58:03 +0100

    IRIS-183 Improved data management of the contact folders.

--- src/idb/IContactsIdb.ts
+++ src/idb/IContactsIdb.ts
@@ -1,6 +1,17 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2020 ZeXtras
+ *
+ * The contents of this file are subject to the ZeXtras EULA;
+ * you may not use this file except in compliance with the EULA.
+ * You may obtain a copy of the EULA at
+ * http://www.zextras.com/zextras-eula.html
+ * *** END LICENSE BLOCK *****
+ */
+
 import { DBSchema } from 'idb';
-import { ContactaddressType, ContactPhoneType } from './ContactEnums';
 import { IFolderSchmV1 } from '@zextras/zapp-shell/lib/sync/IFolderSchm';
+import { ContactaddressType, ContactPhoneType } from './ContactEnums';
 
 export interface IContactsIdb extends DBSchema {
 	contacts: {
@@ -8,7 +19,7 @@ export interface IContactsIdb extends DBSchema {
 		value: Contact;
 		indexes: {
 			parent: string;
-		}
+		};
 	};
 	folders: {
 		key: string;
